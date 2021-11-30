0aec95e1c71302beddd831877de1338207bdf544
PabloNahabedian <89050566+PabloNahabedian@users.noreply.github.com>
2021-11-30 19:48:32 -0300

    NavbarComp actualizado
    Agregué las secciones y cambié el color del nav.

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -5,9 +5,9 @@ export default class NavbarComp extends Component {
     render() {
         return (
             <div>
-                <Navbar bg="light" expand="lg">
+                <Navbar bg="dark" variant={"dark"} expand="lg">
   <Container fluid>
-    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+    <Navbar.Brand href="#">Tu tienda</Navbar.Brand>
     <Navbar.Toggle aria-controls="navbarScroll" />
     <Navbar.Collapse id="navbarScroll">
       <Nav
@@ -15,18 +15,15 @@ export default class NavbarComp extends Component {
         style={{ maxHeight: '100px' }}
         navbarScroll>
         <Nav.Link href="#action1">Home</Nav.Link>
-        <Nav.Link href="#action2">Link</Nav.Link>
-        <NavDropdown title="Link" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+        <Nav.Link href="#action2">Galería</Nav.Link>
+        <NavDropdown title="Categorías" id="navbarScrollingDropdown">
+          <NavDropdown.Item href="#action3">Categoría 1</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Categoría 2</NavDropdown.Item>
           <NavDropdown.Divider />
           <NavDropdown.Item href="#action5">
-            Something else here
+            Ver todas las categorías
           </NavDropdown.Item>
         </NavDropdown>
-        <Nav.Link href="#" disabled>
-          Link
-        </Nav.Link>
       </Nav>
       <Form className="d-flex">
         <FormControl
